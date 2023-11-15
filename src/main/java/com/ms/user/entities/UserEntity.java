4ab80f6032d507ffebbcebc5421e964a443ed668
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-11-14 21:20:27 -0300

    feat: class User

---
+++ src/main/java/com/ms/user/entities/UserEntity.java
@@ -0,0 +1,25 @@
+package com.ms.user.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USER")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+    private String password;
+}
